9f9d45ccc0cf706da264afee049ac7c94cb565a3
phbelmiro <142456085+phbelmiro@users.noreply.github.com>
2023-09-15 21:48:58 -0300

    Create aula-final.js

---
+++ aula-final.js
@@ -0,0 +1,78 @@
+// while(condiçao){
+
+// }
+
+// let i = 0;
+// while (i < 10) {
+    
+//     console.log(i);
+    
+//     i++;
+// }
+
+
+// let estomago = 0;
+
+// while(estomago < 100){
+//     console.log("quero comer mais coxinhas")
+//     estomago = estomago + 10
+// }
+
+
+
+
+// let numeros = 0;
+// let numero = Number(prompt("Digite um numero"));
+
+// while(numero !== 0){
+//    numeros = numeros + numero;
+//    numero = Number(prompt("Digite um numero")) 
+// }
+
+// console.log("A soma dos numero inseridos e: " + numeros)
+
+// const numeros =[14, 67, 89, 15, 23]
+
+// for (let i = 0; i<5; i++) {
+//     const Elemento = numeros[i]
+//     console.log(Elemento)
+// }
+
+
+
+// function maiorElemento(lista){
+//     let numero = lista[0];
+//     for (let i = 0; i < lista.length; i++) {
+//         if(lista[i]> numero){
+//         numero = lista[i]
+//         }
+//     }
+//     return "O maior numero e " + numero
+// }
+// const numeros = [11, 15, 18, 14, 90, 13]
+// console.log(maiorElemento(numeros))
+
+// const numeros = [14, 64, 89, 15, 23];
+
+// for (let numero of numeros){
+//     console.log(numero);
+// }
+
+// numeros.forEach(numero =>{
+//     console.log
+//     (numero)
+// });
+
+let palavras = ["ola", "amigo" ,"como", "voce","esta?"]
+
+function retornafrasecompleta(arraydepalavras) {
+let fras =" ";
+for(let i = 0 ; i < arraydepalavras.length; i++){
+    const Elemento=arraydepalavras[i];
+    fs = fs + " " + Elemento;
+}
+    return fs1;
+    
+}
+1
+console.log(funcao(palavras))
